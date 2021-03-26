848335f88c1a7d117766833ed129795b05f4ea96
croissantderp <70451529+croissantderp@users.noreply.github.com>
2021-03-25 19:04:20 -0700

    QUANTUM

---
+++ Commands/qeckoAccess.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using static System.Diagnostics.Debug;
+using Discord;
+using Discord.Commands;
+
+using Quantum.qeckoBot;
+
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace GeckoBot.Commands
+{
+    public class QuantumStart : ModuleBase<SocketCommandContext>
+    {
+        //rng but qUaNtUm
+        [Command("qrng")]
+        [Summary("random function using quantum bits")]
+        public async Task qrng()
+        {
+            using var sim = new QuantumSimulator();
+
+            long num = await Quantum.qeckoBot.SampleRandomNumber.Run(sim);
+            await ReplyAsync(num.ToString());
+        }
+    }
+}
+
